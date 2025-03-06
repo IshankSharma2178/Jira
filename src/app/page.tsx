fa1c958bbbe934ee8633628f5e14185eb7ae27ef
IshankSharma2178 <ishank.sharma.9891@gmail.com>
2025-03-06 20:59:47 +0530

    Customize button styles and input size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Input />
+      <Button variant={"primary"}>Primary</Button>
+      <Button variant={"destructive"}>destructive</Button>
+      <Button variant={"secondary"}>secondary</Button>
+      <Button variant={"ghost"}>ghost</Button>
+      <Button variant={"muted"}>muted</Button>
+      <Button variant={"outline"}>outline</Button>
+      <Button variant={"teritary"}>teritary</Button>
+    </div>
+  );
 }
