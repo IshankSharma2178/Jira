04f4cc6cd86c0d4b8c62f723a3e8cdb79d076314
IshankSharma2178 <ishank.sharma.9891@gmail.com>
2025-03-07 13:12:19 +0530

    Add authentication pages and validation schemas

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { SignInCard } from "@/features/auth/components/Sign-In-Card";
+import React from "react";
+
+const SignInPage = () => {
+  return <SignInCard />;
+};
+
+export default SignInPage;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { SignUpCard } from "@/features/auth/components/Sign-Up-Card";
+import React from "react";
+
+const SignUpPage = () => {
+  return <SignUpCard />;
+};
+
+export default SignUpPage;
