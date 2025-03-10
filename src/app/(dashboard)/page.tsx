7e002cf9adc833087b41edb23124bb5af389f591
IshankSharma2178 <ishank.sharma.9891@gmail.com>
2025-03-10 18:34:53 +0530

    Implement dashboard layout with navigation and sidebar components; replace home page authentication handling

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,13 +1,8 @@
 import { protect } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
 import { redirect } from "next/navigation";
 export default async function Home() {
   const user = await protect();
   if (!user) redirect("/sign-in");
 
-  return (
-    <div>
-      <UserButton />
-    </div>
-  );
+  return <div>This is our home page</div>;
 }
