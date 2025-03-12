7ccba727308ec885422adcff20093070a2ea9b2b
IshankSharma2178 <ishank.sharma.9891@gmail.com>
2025-03-12 12:53:10 +0530

    Add members management: introduce MEMBERS_ID config, implement member role handling, and update workspace retrieval logic

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,15 +2,34 @@ import { Hono } from "hono";
 import { zValidator } from "@/features/middleware/validation.middleware";
 import { workSpaceSchema } from "@/validation/workspace-schema/workspace-validation";
 import { sessionMiddleware } from "@/features/middleware/session.middleware";
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
-import { ID } from "node-appwrite";
+import {
+  DATABASE_ID,
+  IMAGES_BUCKET_ID,
+  MEMBERS_ID,
+  WORKSPACES_ID,
+} from "@/config";
+import { ID, Query } from "node-appwrite";
+import { MemberRole } from "@/features/members/type";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
     const databases = c.get("databases");
+    const user = c.get("user");
+
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      Query.equal("userId", user.$id),
+    ]);
+
+    if (members.total === 0) {
+      return c.json({ data: { documents: [], total: 0 } });
+    }
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
+
     const workspaces = await databases.listDocuments(
       DATABASE_ID,
-      WORKSPACES_ID
+      WORKSPACES_ID,
+      [Query.orderDesc("$createdAt"), Query.contains("$id", workspaceIds)]
     );
     return c.json({ data: workspaces });
   })
@@ -48,6 +67,13 @@ const app = new Hono()
         ID.unique(),
         { name, userId: user.$id, imageUrl: uploadImageUploadUrl }
       );
+
+      await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+        userId: user.$id,
+        workspaceId: workspace.$id,
+        role: MemberRole.ADMIN,
+      });
+
       return c.json({ data: workspace });
     }
   );
